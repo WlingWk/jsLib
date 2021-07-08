bee91a08d31a08dd80ab0c303d46d276d748dc52
zqs <wl@juexiaotime.com>
2021-07-08 17:30:04 +0800

    num2Ch

--- num2Ch.js
+++ num2Ch.js
@@ -1,29 +1,22 @@
-num2Ch = (section) => {
-  let index = 0;
-  const chnNumChar = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
-  const chnUnitSection = ["", "万", "亿", "万亿", "亿亿"];
-  const chnUnitChar = ["", "十", "百", "千"];
-  let strIns = "",
-    chnStr = "";
-  let unitPos = 0;
-  let zero = true;
-  while (section > 0) {
-    index++;
-    var v = section % 10;
-    if (v === 0) {
-      if (!zero) {
-        zero = true;
-        chnStr = chnNumChar[v] + chnStr;
-      }
-    } else {
-      zero = false;
-      strIns = chnNumChar[v];
-      if (strIns == "一" && chnUnitChar[unitPos] == "十") strIns = "";
-      strIns += chnUnitChar[unitPos];
-      chnStr = strIns + chnStr;
-    }
-    unitPos++;
-    section = Math.floor(section / 10);
+const num2Ch = function(num) {
+  var arr1 = new Array('零', '一', '二', '三', '四', '五', '六', '七', '八', '九');
+  var arr2 = new Array('', '十', '百', '千', '万', '十', '百', '千', '亿', '十', '百', '千','万', '十', '百', '千','亿');//可继续追加更高位转换值
+  if(!num || isNaN(num)){
+      return "零";
   }
-  return chnStr;
+  var english = num.toString().split("")
+  var result = "";
+  for (var i = 0; i < english.length; i++) {
+      var des_i = english.length - 1 - i;//倒序排列设值
+      result = arr2[i] + result;
+      var arr1_index = english[des_i];
+      result = arr1[arr1_index] + result;
+  }
+  result = result.replace(/零(千|百|十)/g, '零').replace(/十零/g, '十');
+  result = result.replace(/零+/g, '零');
+  result = result.replace(/零亿/g, '亿').replace(/零万/g, '万');
+  result = result.replace(/亿万/g, '亿');
+  result = result.replace(/零+$/, '')
+  result = result.replace(/^一十/g, '十');
+  return result;
 }
